009370daab3172e0093d48c51f06ab4106df1c13
Mac <mac@Macs-MacBook-Pro.local>
2026-04-09 19:36:26 +0200

    Mina izmena

--- src/Main.java
+++ src/Main.java
@@ -4,11 +4,13 @@ public class Main {
 
         Roman ratIMir = new Roman("Rat i mir" , "Tolstoj", 832, "novela, istorijski");
         SkolskaKnjiga svetOkoNas = new SkolskaKnjiga ("Matematika 2"  , "Sanja Blagdanić", 120, "Matematika" );
+        SkolskaKnjiga engleski = new SkolskaKnjiga ("Engleski 2"  , "Jelena M", 120, "Engleski" );
 
         ratIMir.infoKnjige();
         svetOkoNas.infoKnjige();
+        System.out.println("Mina");
 
-
+        
 
     }
 }
